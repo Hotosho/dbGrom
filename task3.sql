8963284c620a13967c957c302c0132b2b7951199
Hotosho <nicksecond1995@gmail.com>
2021-08-20 00:43:27 +0200

    t3

---
+++ task3.sql
@@ -0,0 +1,102 @@
+delete from orders
+SELECT * FROM public.orders
+
+insert into orders VALUES
+(1001, 'test1', 500, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1002, 'test2', 600, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1003, 'test3', 300, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1004, 'test4', 1000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1005, 'test5', 5000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1006, 'test6', 2500, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1007, 'test7',70000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1008,'test8', 250, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1009, 'test9', 950,to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1010, 'test10', 12000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss'))
+
+alter table product rename to products
+SELECT * FROM public.products
+
+insert into products values
+(110, 'name1', 'bmw', 10),
+(120,'name2','audi', 20),
+(130, 'name3','mers',30),
+(140, 'name4', 'toyota', 40),
+(150, 'name5', 'suzuki', 50),
+(160, 'name6', 'porshe',60),
+(170, 'name7', 'jeep', 70),
+(180, 'name8', 'volsfagen',80),
+(190,'name9','opel', 90),
+(200, 'name10', 'tesla', 100)
+
+select * from products where cost_price = 50 and name_product = 'name5'
+select * from products where id =150 or cost_price < 50
+select * from products where id > 150 and from delete from orders
+SELECT * FROM public.orders
+
+insert into orders VALUES
+(1001, 'test1', 500, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1002, 'test2', 600, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1003, 'test3', 300, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1004, 'test4', 1000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1005, 'test5', 5000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1006, 'test6', 2500, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1007, 'test7',70000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1008,'test8', 250, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1009, 'test9', 950,to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1010, 'test10', 12000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss'))
+
+alter table product rename to products
+SELECT * FROM public.products
+
+insert into products values
+(110, 'name1', 'bmw', 10),
+(120,'name2','audi', 20),
+(130, 'name3','mers',30),
+(140, 'name4', 'toyota', 40),
+(150, 'name5', 'suzuki', 50),
+(160, 'name6', 'porshe',60),
+(170, 'name7', 'jeep', 70),
+(180, 'name8', 'volsfagen',80),
+(190,'name9','opel', 90),
+(200, 'name10', 'tesla', 100)
+
+select * from products where cost_price = 50 and name_product = 'name5'
+select * from products where id =150 or cost_price < 50
+select * from products where id > 150 and delete from orders
+SELECT * FROM public.orders
+
+insert into orders VALUES
+(1001, 'test1', 500, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1002, 'test2', 600, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1003, 'test3', 300, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1004, 'test4', 1000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1005, 'test5', 5000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1006, 'test6', 2500, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1007, 'test7',70000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1008,'test8', 250, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1009, 'test9', 950,to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss')),
+(1010, 'test10', 12000, to_date('2020/05/03', 'yyyy/mm/dd hh24:mi:ss'), to_date('2021/04/04', 'yyyy/mm/dd hh24:mi:ss'))
+
+alter table product rename to products
+SELECT * FROM public.products
+
+insert into products values
+(110, 'name1', 'bmw', 10),
+(120,'name2','audi', 20),
+(130, 'name3','mers',30),
+(140, 'name4', 'toyota', 40),
+(150, 'name5', 'suzuki', 50),
+(160, 'name6', 'porshe',60),
+(170, 'name7', 'jeep', 70),
+(180, 'name8', 'volsfagen',80),
+(190,'name9','opel', 90),
+(200, 'name10', 'tesla', 100)
+
+select * from products where cost_price = 50 and name_product = 'name5'
+select * from products where id =150 or cost_price < 50
+select * from products where id > 150
+
+update products set name_product = 'audi-T-shirt' where id = 120
+update products set manufacturer_name = 'china'
+
+delete from products where id < 150
