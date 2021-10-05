45bdbf3df13c2f84d3f6b8d80b47dcb7dc966925
Mykyta Kazimirov <nicksecond1995@gmail.com>
2021-10-05 02:50:23 +0200

    Delete forumPollsOptions.sql

--- task6/forumPollsOptions.sql
+++
@@ -1,8 +0,0 @@
-create table forumPollsOptions(
-id integer, 
-	constraint idForumPollsOptions_pk primary key(id),
-	idPost integer,
-	constraint idPost_fk foreign key (idPost) references forumPost(id),
-	title varchar(64),
-	date_forumPollsOptions timestamp
-)
