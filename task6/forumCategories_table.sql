fd4da2d949a11d819cfc4337e958868fc3622c9e
Mykyta Kazimirov <nicksecond1995@gmail.com>
2021-10-05 02:50:11 +0200

    Delete forumCategories_table.sql

--- task6/forumCategories_table.sql
+++
@@ -1,8 +0,0 @@
-create table forumCategories(
-id integer,
-	constraint idCategories_pk primary key (id),
-	title varchar(64),
-	description text,
-	date_forumCategories timestamp,
-	ip varchar(64)
-)
